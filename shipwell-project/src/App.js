1b3fb8a5792b670711474a7262e94be1a9802681
Jeremy <jordanudis@yahoo.com>
2017-10-03 12:39:19 -0600

    update google maps componenets

--- shipwell-project/src/App.js
+++ shipwell-project/src/App.js
@@ -3,6 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address1: '',
+      address2: '',
+    }
+    this.initMap = this.initMap.bind(this);
+  }
+  // componentDidMount() {
+  //  var map;
+  //  map = new google.maps.Map(this.refs.map, {
+  //     center: {lat: -34.397, lng: 150.644},
+  //     zoom: 8
+  //   });
+  //  return map;
+  // }
+  initMap() { 
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+    return map;
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +36,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+        <div id="map"></div>
+          <script>
+            {this.initMap}   
+          </script>
+                  
+            
+       </div>
+        
     );
   }
 }
